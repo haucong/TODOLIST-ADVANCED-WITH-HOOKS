48e3042b1bb1f8141a27d94590ce3f1d231d3f4d
haucong <ngoconghau65@gmail.com>
2021-11-24 23:13:26 +0700

    add useReducer+

--- src/context/AuthContext.js
+++ src/context/AuthContext.js
@@ -1,19 +1,20 @@
-import React,{createContext, useEffect, useState} from 'react';
+import React,{createContext, useEffect, useReducer, useState} from 'react';
+import { AuthReducer } from '../Reducer/AuthReducer';
 export const AuthContext = createContext();
 
 const AuthContextProvider = ({children}) =>{
-    const [isAuthenticated, setAuthentication] = useState(false)
-    const toggleAuth = () => {
-        setAuthentication(!isAuthenticated)
-    }
-
+    // const [isAuthenticated, setAuthentication] = useState(false)
+    // const toggleAuth = () => {
+    //     setAuthentication(!isAuthenticated)
+    // }
+    const [isAuthenticated, dispatch] = useReducer(AuthReducer, false)
     useEffect(() => {
         alert(isAuthenticated ? 'Loggin successfully' : 'You are logged out.Please Login to see todo')
     }, [isAuthenticated])
 
     const AuthenticatedData = {
       isAuthenticated,
-      toggleAuth,
+      dispatch
     }
 
     return (
